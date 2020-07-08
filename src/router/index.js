46b0966e1f0260a56c21fa99bac766b1793ff8c4
kyawthura-gg <ggaryal@gmail.com>
2020-07-08 21:59:55 +0630

    updated router

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,23 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Cluster from "../views/Cluster.vue";
+import Graphs from "../views/Graphs.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
+const routes = [{
     path: "/",
     name: "Home",
     component: Home
+  }, {
+    path: "/cluster",
+    name: "Graphs",
+    component: Graphs
+  }, {
+    path: "/graphs",
+    name: "Graphs",
+    component: Graphs
   },
   {
     path: "/about",
@@ -17,7 +26,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import( /* webpackChunkName: "about" */ "../views/About.vue")
   }
 ];
 
