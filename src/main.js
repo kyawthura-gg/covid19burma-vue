ee60d6ff490ef12df56d311bb87c647ff1a76de1
kyawthura-gg <ggaryal@gmail.com>
2020-07-08 22:00:07 +0630

    updated main js

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueAxios from "vue-axios";
 import axios from "axios";
+import MyanmarMap from "@/components/MyanmarMap.vue";
 
 Vue.config.productionTip = false;
-axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
-// window.$ = window.jQuery = require('./assets/js/jquery.js');
-
+Vue.component("MyanmarMap", MyanmarMap);
+Vue.use(VueAxios, axios);
 new Vue({
   router,
   store,
