b9ca4daa56079375a0e9d15857a8a83fd31f53c6
kyawthura-gg <ggaryal@gmail.com>
2020-08-01 16:46:24 +0630

    Total report for cases

--- src/components/ReportByDate.vue
+++ src/components/ReportByDate.vue
@@ -1,92 +1,48 @@
 <template>
-  <canvas id="reportDate" width="400" height="400"></canvas>
+  <ve-line :data="chartData" :settings="chartSettings"></ve-line>
 </template>
+
 <script>
-var byStateTitle = "Confirmed cases by states";
-var dailyTitle = "Daily Cases";
-var confirmed = "Confirmed";
-var deaths = "Deaths";
-var recovered = "Recovered";
-var DateConfirm = new Array();
-var DateLabels = new Array();
-var DateDeaths = new Array();
-var DateRecovered = new Array();
-var urlDate = "https://thantthet.github.io/covid19-api/data.json";
-$(document).ready(function() {
-  $.get(urlDate, function(response) {
-    var caseTime = response.cases_time_series;
-    caseTime.forEach(function(data) {
-      DateConfirm.push(data.totalconfirmed);
-      DateLabels.push(data.date);
-      DateDeaths.push(data.totaldeceased);
-      DateRecovered.push(data.totalrecovered);
-    });
-    var ctx = document.getElementById("reportDate").getContext("2d");
-    var myChart = new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: DateLabels,
-        datasets: [
-          {
-            label: confirmed,
-            fill: true,
-            backgroundColor: "hsl(348, 100%, 61%)",
-            data: DateConfirm,
-          },
-          {
-            label: deaths,
-            fill: true,
-            backgroundColor: "hsl(0, 0%, 86%)",
-            data: DateDeaths,
-          },
-          {
-            label: recovered,
-            fill: true,
-            backgroundColor: "hsl(171, 100%, 41%)",
-            data: DateRecovered,
-          },
+export default {
+  data() {
+    this.chartSettings = {
+      // area: true,
+      labelMap: {
+        totalrecovered: "Recovered",
+        active: "Active",
+        totaldeceased: "Deaths",
+        totalconfirmed: "Confirmed"
+      }
+    };
+    return {
+      chartData: {
+        columns: [
+          "date",
+          "totalrecovered",
+          "active",
+          "totaldeceased",
+          "totalconfirmed"
         ],
-      },
-      options: {
-        title: {
-          display: true,
-          text: dailyTitle,
-          fontColor: "white",
-          fontSize: 17,
-        },
-        legend: {
-          display: true,
-          labels: {
-            fontColor: "white",
-          },
-        },
-        scales: {
-          xAxes: [
-            {
-              stacked: true,
-              gridLines: {
-                display: false,
-              },
-            },
-          ],
-          yAxes: [
-            {
-              stacked: true,
-              ticks: {
-                fontColor: "white",
-                fontSize: 14,
-                // stepSize: 1,
-                // beginAtZero: true
-              },
-              gridLines: {
-                display: false,
-              },
-            },
-          ],
-        },
-      },
-    });
-  });
-});
-export default {};
+        rows: [],
+        textStyle: {
+          color: "#fff",
+          fontSize: 12
+        }
+      }
+    };
+  },
+  mounted() {
+    this.fetchMapCases();
+  },
+  methods: {
+    async fetchMapCases() {
+      this.axios
+        .get(`https://thantthet.github.io/covid19-api/data.json`)
+        .then(response => {
+          var caseTime = response.data.cases_time_series;
+          this.chartData.rows = caseTime;
+        });
+    }
+  }
+};
 </script>
