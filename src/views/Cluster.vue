860f2a4d24b75cc4aacd1d7fad416c0b7acceeed
kyawthura-gg <ggaryal@gmail.com>
2020-08-02 21:46:16 +0630

    Updated Cluster with echart

--- src/views/Cluster.vue
+++ src/views/Cluster.vue
@@ -1,3 +1,63 @@
 <template>
-  <div style="margin-top: 200px; color: white; font-size: 30px; font-weight: bold;">Coming Soon!</div>
+  <div class="content" id="cluster-canvas" style="width: 100%;height:600px;"></div>
 </template>
+<script>
+import echarts from "echarts";
+export default {
+  mounted() {
+    this.loadCluster();
+  },
+  methods: {
+    loadCluster() {
+      var myChart = echarts.init(document.getElementById("cluster-canvas"));
+      $.get("graph.json", function(webkitDep) {
+        var option = {
+          tooltip: {},
+          title: {
+            text: "Patient to Patient Link Graph",
+            subtext: "Default layout",
+            top: "bottom",
+            left: "right"
+          },
+          legend: {
+            data: ["Male", "Female", "Unknown"]
+          },
+          animationDuration: 1500,
+          animationEasingUpdate: "quinticInOut",
+          series: [
+            {
+              name: "Patient to Patient Link Graph",
+              type: "graph",
+              layout: "force",
+              animation: false,
+              label: {
+                position: "right",
+                formatter: "{b}"
+              },
+              roam: true,
+              focusNodeAdjacency: true,
+              draggable: false,
+              data: webkitDep.nodes.map(function(node, idx) {
+                node.id = idx;
+                return node;
+              }),
+              categories: webkitDep.categories,
+              force: {
+                edgeLength: 5,
+                repulsion: 40,
+                gravity: 0.2
+              },
+              edges: webkitDep.links,
+              lineStyle: {
+                color: "source",
+                curveness: 0
+              }
+            }
+          ]
+        };
+        myChart.setOption(option);
+      });
+    }
+  }
+};
+</script>
