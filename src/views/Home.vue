df1e465d5b7e7c375356babe79841dcb367226d5
kyawthura-gg <ggaryal@gmail.com>
2020-12-21 14:38:27 +0630

    fixed typo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <div class="message is-warning">
         <div class="message-body">
           <strong
-            >Due to high number of cases, following data might not be correct.
+            >Due to high number of cases, following data might not correct.
             Please check to offical website
             <a href="https://www.mohs.gov.mm" target="_blank"> mohs.gov.mm </a>
           </strong>
